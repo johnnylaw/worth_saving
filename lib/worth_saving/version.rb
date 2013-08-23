369e059330c01ad89fea9e86801ddfd3b1a8207e
Allen Weiss, John Lawrence & Trace Wax <pair+allen+jlaw+trace@sweetyhigh.com>
2013-08-22 23:37:03 -0700

    Initial commit

---
+++ lib/worth_saving/version.rb
@@ -0,0 +1,3 @@
+module WorthSaving
+  VERSION = "0.0.1"
+end
---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = 'd626dd39e73252a931ee0348831e23f279a7d9d82f93a614a1b7c95513b50f036ef43f90a626e15571c0905778502d853979f502e217a79361d5c3e08b1f21f4'

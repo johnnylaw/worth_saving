23980a35d57d925b9ff4f11e87687bb74f0d1ccd
John Lawrence <johnonrails@gmail.com>
2013-08-27 00:03:35 -0700

    Remove unnecessary lines from spec_helper and main file

--- lib/worth_saving.rb
+++ lib/worth_saving.rb
@@ -1,8 +1,1 @@
-require 'active_record'
 require 'worth_saving/active_record_ext'
-require 'worth_saving/action_view_ext'
-
-require 'generators/worth_saving'
-require 'generators/worth_saving/install/install_generator'
-
-# ActiveRecord::Base.send :include, WorthSaving::ActiveRecordExt

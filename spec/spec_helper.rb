e87cf2fe7c7bd1579e3057548ca3ad76b6d0a86a
Allen Weiss & John Lawrence <pair+allen+jlaw@sweetyhigh.com>
2013-08-26 22:52:45 -0700

    Remove unnecessary lines from spec_helper and main file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,12 @@
 require 'rubygems'
 require 'pry'
-require 'factory_girl'
-require File.expand_path '../factories', __FILE__
 require 'with_model'
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
+# require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.

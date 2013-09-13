ae66ff436eb4075fa1030477c0e91127e95386c4
John Lawrence <johnonrails@gmail.com>
2013-09-13 04:43:53 -0700

    Add support for authorization

--- app/controllers/worth_saving/application_controller.rb
+++ app/controllers/worth_saving/application_controller.rb
@@ -1,4 +1,4 @@
 module WorthSaving
-  class ApplicationController < ActionController::Base
+  class ApplicationController < ::ApplicationController
   end
 end
--- app/controllers/worth_saving/drafts_controller.rb
+++ app/controllers/worth_saving/drafts_controller.rb
@@ -1,13 +1,13 @@
 class WorthSaving::DraftsController < WorthSaving::ApplicationController
   def create
     @draft = WorthSaving::Draft.new draft_params
-    render_result
+    save_if_authorized_and_respond
   end
 
   def update
     @draft = WorthSaving::Draft.find params[:id]
     @draft.attributes = draft_params
-    render_result
+    save_if_authorized_and_respond
   end
 
   private
@@ -20,11 +20,32 @@ class WorthSaving::DraftsController < WorthSaving::ApplicationController
     end
   end
 
-  def render_result
+  def save_if_authorized_and_respond
+    authorized? ? save_and_respond : render_forbidden_error
+  end
+
+  def save_and_respond
     if @draft.save
       render json: { message: 'Draft saved', worthSavingDraft: { action: worth_saving.draft_path(@draft) } }
     else
       render json: { error: 'Unable to save draft' }
     end
+  end
+
+  def render_forbidden_error
+    render status: 403, text: "Not authorized to draft this #{@draft.recordable_type}"
+  end
+
+  def authorized?
+    return authorized_to_draft_record?(@draft.recordable) if @draft.recordable
+    authorized_to_draft_with_scope?(@draft.scopeable)
+  end
+
+  def authorized_to_draft_record?(recordable)
+    defined?(super) ? super : true
+  end
+
+  def authorized_to_draft_with_scope?(scopeable)
+    defined?(super) ? super : true
   end
 end

a99842e011c1aab52b99f6c434212fdbb408cc2d
John Lawrence <johnonrails@gmail.com>
2013-09-12 21:48:08 -0700

    Add callback to worth_saving record classes
    - to destroy worth_saving_draft

--- lib/worth_saving/active_record_ext.rb
+++ lib/worth_saving/active_record_ext.rb
@@ -7,11 +7,21 @@ module WorthSaving
 
     module ClassMethods
       def is_worth_saving(opts = {})
-        has_one :worth_saving_draft, class_name: 'WorthSaving::Draft', as: :recordable
-        @worth_saving_info = WorthSaving::Info.new self, opts
+        class_eval do
+          has_one :worth_saving_draft, class_name: 'WorthSaving::Draft', as: :recordable
+          after_save :destroy_worth_saving_draft
 
-        def self.worth_saving_info
-          @worth_saving_info
+          @worth_saving_info = WorthSaving::Info.new self, opts
+
+          def self.worth_saving_info
+            @worth_saving_info
+          end
+
+          private
+
+          def destroy_worth_saving_draft
+            worth_saving_draft && worth_saving_draft.destroy
+          end
         end
       end
 

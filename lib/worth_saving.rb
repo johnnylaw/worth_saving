c97582d5aac26340db224bb9b32a09983d6525b3
John Lawrence <johnonrails@gmail.com>
2013-09-17 09:36:42 -0700

    Add config options for form (interval, message)

--- lib/worth_saving.rb
+++ lib/worth_saving.rb
@@ -5,5 +5,8 @@ require 'worth_saving/form_builder'
 require 'worth_saving/form'
 require 'worth_saving/action_view_ext'
 
+module WorthSaving
+end
+
 ActiveRecord::Base.send :include, WorthSaving::ActiveRecordExt
 ActionView::Base.send :include, WorthSaving::ActionViewExt

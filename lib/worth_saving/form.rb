002247415ee472ffa7f2ee2a2982681ec4abc76b
John Lawrence <johnonrails@gmail.com>
2013-09-17 09:36:42 -0700

    Add config options for form (interval, message)

--- lib/worth_saving/form.rb
+++ lib/worth_saving/form.rb
@@ -4,7 +4,6 @@ require 'worth_saving/form/base'
 
 module WorthSaving
   module Form
-    DEFAULT_INTERVAL = 60
     WORTH_SAVING_DRAFT_FORM_BLOCK = proc { |f|
       f.hidden_field(:recordable_id) +
       f.hidden_field(:recordable_type) +
@@ -12,9 +11,5 @@ module WorthSaving
       f.hidden_field(:scopeable_type) +
       f.hidden_field(:form_data)
     }
-    RECOVERY_MSG = <<EOS
-It appears you were working on a draft that didn't get saved.
-It is recommended that you choose one now, as no more draft copies will be saved until you do.
-EOS
   end
 end

5cf6866eb5822c6e8df346aa6a17d224cbe3397a
John Lawrence <johnonrails@gmail.com>
2013-09-18 01:18:27 -0700

    Add feature specs

--- spec/features/worth_saving_features_spec.rb
+++ spec/features/worth_saving_features_spec.rb
@@ -1,25 +1,60 @@
 require 'spec_helper'
 
 feature 'WorthSaving' do
-  scenario "edits done to a new page will appear in a recovery form when refreshing the page", js: true do
-    fred, barney = given_users('Fred Flinstone', 'Barney Rubble')
+  let(:fred) { given_user 'Fred Flintstone' }
+  let(:barney) { given_user 'Barney Rubble' }
+
+  scenario "A user begins work on a new page and sees recovery option when refreshing the page", js: true do
     login fred
     visit new_page_path
     within '.worth-saving-standard-form-container .worth-saving-form-container' do
       fill_in_form
     end
     chill_out_long_enough_to_draft
-
     visit current_path
-
-    # expect the recovery message
-
+    within '.worth-saving-header-message' do
+      expect(page).to have_content "It appears you were working on a draft that didn't get saved"
+    end
     within '.worth-saving-recovery-form-container #new_page' do
       verify_blank_form
     end
-
     within '.worth-saving-recovery-form-container #recovered_new_page' do
       verify_form({}, true)
+    end
+  end
+
+  scenario "As a user I start work on a new page, refresh and see draft, but another user can't see it", js: true do
+    login fred
+    visit new_page_path
+    within '.worth-saving-standard-form-container .worth-saving-form-container' do
+      fill_in_form
+    end
+    chill_out_long_enough_to_draft
+    visit current_path
+    within '.worth-saving-header-message' do
+      expect(page).to have_content "It appears you were working on a draft that didn't get saved"
+    end
+    login barney
+    visit new_page_path
+    page.should_not have_css '.worth-saving-header-message'
+  end
+
+  scenario "One user creates a page.  Another edits that page, and drafts do not get saved because of methods in ApplicationController", js: true do
+    login fred
+    visit new_page_path
+    within '.worth-saving-standard-form-container .worth-saving-form-container' do
+      fill_in_form
+    end
+    click_on 'Create Page'
+    path = current_path
+    login barney
+    visit path
+    within '.worth-saving-standard-form-container .worth-saving-form-container' do
+      fill_in 'Content', with: "Here is some new content that I will not expect to see"
+    end
+    chill_out_long_enough_to_draft
+    within '.worth-saving-form-message.error' do
+      page.should have_content "Not authorized to draft this Page"
     end
   end
 end

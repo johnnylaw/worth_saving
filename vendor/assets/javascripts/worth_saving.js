ad6938dc2a63567855bbdebb20e850d5cb15d0fc
John Lawrence <johnonrails@gmail.com>
2013-10-02 00:19:25 -0700

    Add CKEditor support to gem

--- vendor/assets/javascripts/worth_saving.js
+++ vendor/assets/javascripts/worth_saving.js
@@ -1,5 +1,6 @@
 //= require_self
 //= require ./worth_saving/tinymce_init
+//= require ./worth_saving/ckeditor_init
 //= require ./worth_saving/form
 
 WorthSaving = {};
@@ -12,4 +13,18 @@ $(document).ready(function() {
 
 WorthSaving.prepareThirdPartyEditorsForSave = function() {
   if(typeof tinymce !== 'undefined') tinymce.triggerSave();
+  if(typeof CKEDITOR !== 'undefined') {
+    for ( instance in CKEDITOR.instances )
+        CKEDITOR.instances[instance].updateElement();
+  }
+};
+
+WorthSaving.initializeThirdPartyEditors = function(startCountdownFn) {
+  if(typeof tinymce !== 'undefined') {
+    WorthSaving.tinymceInit(startCountdownFn);
+  }
+
+  if(typeof CKEDITOR !== 'undefined') {
+    WorthSaving.ckeditorInit(startCountdownFn);
+  }
 };
